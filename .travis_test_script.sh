285bd5ba2bac986ec584c8586ed2cb9189bb3130
Raphaël Valyi <rvalyi@gmail.com>
2014-02-10 21:50:33 +0100

    [TRAVIS] testing a few more modules

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ..
-python server/openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=openerp-addons-ci,web/addons -i sale,purchase,stock,crm_claim,project -d rsocb > >(tee stdout.log)
+python server/openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=openerp-addons-ci,web/addons -i sale,purchase,stock,crm_claim,project,event,hr_attendance,hr_contract,hr_expense,hr_timesheet_invoice,mrp,project_mrp -d rsocb > >(tee stdout.log)
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
