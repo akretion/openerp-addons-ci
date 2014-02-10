c644b658f84f4d562e49cd07a9723bdadd03f984
Raphaël Valyi <rvalyi@gmail.com>
2014-02-10 21:12:27 +0100

    [TRAVIS] no test in base addon

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../server
-
-./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i sale,purchase,stock,crm_claim,project -d rsocb > >(tee stdout.log)
+./server/openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=openerp-addons-ci,web/addons -i sale,purchase,stock,crm_claim,project -d rsocb > >(tee stdout.log)
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
