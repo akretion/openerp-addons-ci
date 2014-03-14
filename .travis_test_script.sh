7996e5e4c6df06ff0b189f71241bd845ab9ed26d
Raphaël Valyi <rvalyi@gmail.com>
2014-03-14 16:02:09 +0100

    [TRAVIS-CI] removing document_ftp from tests

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../server
-ADDONS=$(python -c "import os;print ','.join([f for f in os.listdir('../openerp-addons-ci') if 'l10n_' not in f and '.' not in f and 'ldap' not in f and 'google' not in f])")
+ADDONS=$(python -c "import os;print ','.join([f for f in os.listdir('../openerp-addons-ci') if 'l10n_' not in f and '.' not in f and 'ldap' not in f and 'google' not in f and 'document_ftp' not in f])")
 
 ./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i $ADDONS -d ocb > >(tee stdout.log)
 
