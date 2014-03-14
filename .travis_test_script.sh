b818833aff09178317a6cee44730f93ffc84eb9e
Raphaël Valyi <rvalyi@gmail.com>
2014-03-14 11:20:41 +0100

    [TRAVIS-CI] attempt to run all tests except l10n_*, google* and ldap*

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 cd ../server
+ADDONS=$(python -c "import os;print ','.join([f for f in os.listdir('/home/rvalyi/openerp-git/openerp-addons-ci') if 'l10n_' not in f and '.' not in f and 'ldap' not in f and 'google' not in f])")
 
-./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i sale,purchase,stock,crm,project -d ocb > >(tee stdout.log)
+./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i $ADDONS -d ocb > >(tee stdout.log)
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
