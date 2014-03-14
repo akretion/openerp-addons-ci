9bd6748e32629f9e271417713af9177a92b177d3
Raphaël Valyi <rvalyi@gmail.com>
2014-03-14 11:51:05 +0100

    [TRAVIS-CI] --test-enable option for OCB

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -3,7 +3,7 @@
 cd ../server
 ADDONS=$(python -c "import os;print ','.join([f for f in os.listdir('../openerp-addons-ci') if 'l10n_' not in f and '.' not in f and 'ldap' not in f and 'google' not in f])")
 
-./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i $ADDONS -d ocb > >(tee stdout.log)
+./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --test-enable --stop-after-init --addons-path=../openerp-addons-ci,../web/addons -i $ADDONS -d ocb > >(tee stdout.log)
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
